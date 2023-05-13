4d8c428e3c7bf548ebe0ea49a6a415249653e115
Michael <michael2ga3@gmail.com>
2023-05-13 13:07:08 +0300

    chore: refactor UI
    Refactor UI elements to improve user experience. Clean up unused codes for better readability.

--- app/src/main/java/com/github/ttl/manager/MainActivity.java
+++ app/src/main/java/com/github/ttl/manager/MainActivity.java
@@ -29,17 +29,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
