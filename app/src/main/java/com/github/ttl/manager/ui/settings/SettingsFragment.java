57cd31dc5d54fca8c734557d0e915ecb8b535e4f
Michael <michael2ga3@gmail.com>
2023-05-13 20:01:01 +0300

    refactor: move settings related files
    Move settings related files to a separate package and update related imports and references.

--- app/src/main/java/com/github/ttl/manager/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/github/ttl/manager/ui/settings/SettingsFragment.java
@@ -1,34 +1,13 @@
-package com.github.ttl.manager.ui.gallery;
+package com.github.ttl.manager.ui.settings;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.PreferenceFragmentCompat;
 import com.github.ttl.manager.R;
 
-import com.github.ttl.manager.databinding.FragmentSettingsBinding;
-
-import java.util.prefs.Preferences;
-
 public class SettingsFragment extends PreferenceFragmentCompat {
-
-    private FragmentSettingsBinding binding;
-
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
     }
 }
