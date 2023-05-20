6a303fc1af194fbe2fff83028556f0711269b82f
Michael <michael2ga3@gmail.com>
2023-05-20 23:52:43 +0300

    feat: add boot-up receiver for TTL Manager
    Adds a new receiver for handling boot-up events in TTL Manager, with necessary permissions added to AndroidManifest.xml.

---
+++ app/src/main/java/com/github/ttl/manager/receivers/BootUpReceiver.java
@@ -0,0 +1,12 @@
+package com.github.ttl.manager.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootUpReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
